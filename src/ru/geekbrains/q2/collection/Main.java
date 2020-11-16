9d22dd58f1da8bf32b8b725ac8b289b9be801cd6
kavaruDP <dp_for_othe@inbox.ru>
2020-11-17 00:12:47 +0300

    Домашняя работа к уроку 5 (Многопоточность)

--- src/ru/geekbrains/q2/collection/Main.java
+++ src/ru/geekbrains/q2/collection/Main.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 
         // Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
@@ -29,8 +31,91 @@ public class Main {
         pb.add("+79164444444","Sidorov");
         System.out.println(pb.get("Petrov"));
 
+        //количество строк - R, а столбцов - C
+        int R = 5;
+        int C = 6;
+        int[][] arr = new int[R][C];
+        int i =0;
+        int j =0;
+        int k=1;
+        int stop = 0;
+        while (k < 19){
+            i= down(i,j,k,arr,R);
+            System.out.println("k=" + k + " i=" + i + " j=" + j + " down");
+            j++;
+            j= right(i,j,k,arr,C);
+            System.out.println("k=" + k + " i=" + i + " j=" + j + " right");
+            i--;
+            i = up(i,j,k,arr);
+            System.out.println("k=" + k + " i=" + i + " j=" + j + " up");
+            j--;
+            j = left(i,j,k,arr);
+            System.out.println("k=" + k + " i=" + i + " j=" + j + " left");
+            i++;
+        }
+        //Вывод результата
+        for (int x = 0; x < R; x++) {
+            for (int y = 0; y < C; y++) {
+                if (arr[x][y]<10) {
+                    System.out.print(arr[x][y] + "  ");
+                } else {
+                    System.out.print(arr[x][y] + " ");
+                }
+            }
+            System.out.println("");
+        }
     }
 
+    //System.out.println("k=" + k + " i=" + i + " j=" + j + " ss=" + ss);
+
+    private static int down (int i, int j, int value, int[][] arr, int maxArr) {
+        while (i < maxArr && arr[i][j] == 0) {
+            arr[i][j] = value;
+            i++;
+            value++;
+            if (i==maxArr) {
+                i--;
+                break;
+            }
+        }
+        return i;
+    }
+    private static int right (int i, int j, int value, int[][] arr, int maxArr) {
+        while (j < maxArr && arr[i][j] == 0) {
+            arr[i][j] = value;
+            j++;
+            value++;
+            if (j==maxArr) {
+                j--;
+                break;
+            }
+        }
+        return j;
+    }
+    private static int up (int i, int j, int value, int[][] arr) {
+        while (i >= 0 && arr[i][j] == 0) {
+            arr[i][j] = value;
+            i--;
+            value++;
+            if (i==-1) {
+                i++;
+                break;
+            }
+        }
+        return i;
+    }
+    private static int left (int i, int j, int value, int[][] arr) {
+        while (j >=0 && arr[i][j] == 0) {
+            arr[i][j] = value;
+            j++;
+            value++;
+            if (j==-1) {
+                j++;
+                break;
+            }
+        }
+        return i;
+    }
     private static void makeList(ArrayList myList) {
 
         myList.add("zero");
@@ -81,4 +166,5 @@ public class Main {
             System.out.println(o.getKey() + ": " + o.getValue());
         }
     }
+
 }
---
+++ src/ru/geekbrains/q2/thread/Main.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.q2.thread;
+
+public class Main {
+    static final int SIZE = 1000000;
+    static final int HALF = SIZE / 2;
+    float[] arr = new float[SIZE];
+
+
+    public static void main(String[] args) {
+        Main e = new Main();
+        e.method1();
+        e.method2();
+    }
+
+    public void method1() {
+        for (int i=0; i< SIZE; i++) {
+            arr[i] = 1.0f;
+        }
+        long a = System.currentTimeMillis();
+        fillArray (arr, SIZE);
+        System.out.println("method1: " + (System.currentTimeMillis() - a));
+    }
+
+    public void method2() {
+        Main e1 = new Main();
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+        for (int i=0; i< SIZE; i++) {
+            arr[i] = 1.0f;
+        }
+        long a = System.currentTimeMillis();
+        // System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение,
+        // откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
+        // Разбиваем массив
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a2, 0, HALF);
+        // расчет новых значений массива в двух потоках
+        new Thread(() -> e1.fillArray(a1, HALF)).start();
+        new Thread(() -> e1.fillArray(a2, HALF)).start();
+        // Склеиваем массив
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+        System.out.println("method2: " + (System.currentTimeMillis() - a));
+    }
+
+    private void fillArray(float[] arr, int size) {
+        for (int i=0; i< size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+}
